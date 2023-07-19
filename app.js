febf6ba78c35d9820c2c09e1141fb9191599d963
andikayudh <andikayudh@gmail.com>
2023-07-19 13:27:01 +0700

    initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8090;
+
+app.get('/', (req, res) => { res.send('hello') });
+app.get('/about', (req, res) => { res.send('-- about --') });
+app.get('/login', (req, res) => { res.send('-- login --') });
+
+app.get('/api/v1', (req, res) => { res.send({ message: 'test api' }) });
+
+app.listen(port, () => {
+    console.log('--- listen ---', port);
+});
+
