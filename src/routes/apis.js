85d6dad0c20e9d2106cf2a89e971d94c27464529
andikayudh <andikayudh@gmail.com>
2023-07-19 14:54:27 +0700

    sample rest

--- src/routes/apis.js
+++ src/routes/apis.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 
 router.get('/v1', (req, res) => { res.send({ message: 'test api' }) });
+router.get('/v1/list-warga', (req, res) => {
+    const { items } = require('../data/warga.json');
+    res.send(items)
+});
+router.get('/v1/list-warga/:nik', (req, res) => {
+    const { items } = require('../data/warga.json');
+    const item = items.find(m => m.nik === +req.params.nik);
+    res.send(item)
+});
 
 module.exports = router;
